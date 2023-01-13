cb6ba023c26c26efc678d5b3a9504bd0656012df
ImSeon01 <121650379+ImSeon01@users.noreply.github.com>
2023-01-14 00:05:19 +0900

    chap01 소스코드
    프로그래밍 언어와 자바, 이클립스 개발 환경 구축, 자바 프로그램 개발 과정

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,15 @@
+// 패키지 선언
+package sec03.exam01;
+/*
+ * 작성자 : 임선혁
+ * 작성일 : 2023.01.13 
+ */
+
+// 클래스 선언
+public class Hello {
+	// 프로그램 싫행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+	}// end of main
+} // end of class
