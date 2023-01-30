0b2a8dc20158d0218210b5e4c39618d1372fb6d4
ImSeon1 <seonhyeogim01@gmail.com>
2023-01-31 00:00:28 +0900

    chapter05 소스코드
    명령라인 입력, 다차원 배열, 배열 복사, 향상된 for문

---
+++ chap05/src/sec02/exam05/MainStringArrayArgument.java
@@ -0,0 +1,26 @@
+package sec02.exam05;
+
+public class MainStringArrayArgument {
+	public static void main(String[] args) {
+		// args = {}일 경우
+		if(args.length != 2) { // 입력된 데이터 개수가 2개 가 아닐 경우
+			System.out.println(args.length);
+			// 0
+			System.out.println("값의 수가 부족합니다.");
+			// 값의 수가 부족합니다.
+			System.exit(0); // 프로그램 강제 종료
+		}
+		// args = {"10", "20"}일 경우
+		System.out.println(args.length);
+		// 2
+		String strNum1 = args[0]; // 첫 번째 데이터 얻기
+		String strNum2 = args[1]; // 두 번째 데이터 얻기
+		
+		int num1 = Integer.parseInt(strNum1); // 문자열을 정수로 변환
+		int num2 = Integer.parseInt(strNum2); // 문자열을 정수로 변환
+		
+		int result = num1 + num2;
+		System.out.println(num1 + " + " + num2 + " = " + result);
+		// 10 + 20 = 30
+	}
+}
